6ed302c6de8b3e573b223417ba9f5f84921ef6e5
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 22:12:25 +0900

    Update 첫 글자를 대문자로.py

--- 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
@@ -6,3 +6,11 @@ def solution(lines):
 import sys
 lines = sys.stdin.readlines()[1:]
 solution(lines)
+
+#대문자 소문자 관련 메서드
+self.islower(), self.isupper() = T/F를 반환하며 문자열이 대/소문자인지 판단
+self.lower(), self.upper() = 대/소문자로 변환한 문자열 반환
+ex) s[0].upper = 대문자로 바뀐 s[0]만 반환한다 s전체 아님!
+
+#메서드는 아니지만 파이썬 문자열의 특기 문자열 슬라이싱
+[start:end:hop] 유용하게 사용하기!
