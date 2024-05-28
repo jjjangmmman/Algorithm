17f4f35247e73d3b7ca56a42d64e88b04c22a31f
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 21:45:43 +0900

    Update 알파벳 개수.py

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -7,3 +7,14 @@ def solution(s):
 #i/o
 s=input()
 print(solution(s))
+
+#<시간복잡도가 더 큼>
+def solution(s):
+    from collections import Counter
+    c = Counter(s)
+    answer = [c[chr(i+97)] for i in range(26)]
+    return " ".join(map(str,answer))
+    
+#i/o
+s=input()
+print(solution(s))
