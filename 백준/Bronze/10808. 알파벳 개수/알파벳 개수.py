fd4c299ee6e9ec14be4639285e5ff469c1a3bc64
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 21:45:05 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,7 +1,7 @@
 def solution(s):
-    from collections import Counter
-    c = Counter(s)
-    answer = [c[chr(i+97)] for i in range(26)]
+    answer = [0 for _ in range(26)]
+    for ch in s:
+        answer[ord(ch)-97]+=1
     return " ".join(map(str,answer))
     
 #i/o
