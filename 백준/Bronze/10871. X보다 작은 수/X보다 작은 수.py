66eea11377f5f307185e284a4d0a1ea9edc3bdb8
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 18:13:01 +0900

    Update X보다 작은 수.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,10 +1,10 @@
-# 1
+# <1>
 l, std = map(int,input().split())
 arr = list(map(int,input().split()))
 result = [str(a) for a in arr if a<std]
 print(' '.join(result))
 
-# 2
+# <2>
 def solution(A, X):
   answer = []
   for a in A:
