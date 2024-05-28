ed33bd51450d5620a667c674a56d13970a695a7a
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 21:08:13 +0900

    Update 색종이.py

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,11 +1,13 @@
 def solution(n,arr):
-  answer = [[0 for b in range(100)] for a in range(100)]
+  answer = [[0 for b in range(100)] for a in range(100)] 
+  #[[0] * 100 for _ in range(100)]
   for index in range(n):
     x,y= arr[index]
     for i in range(x,x+10):
       for j in range(y,y+10):
         answer[i][j] = 1
   return sum( sum(row) for row in answer)
+  #return sum(map(sum, answer))
     
         
 # i/o
