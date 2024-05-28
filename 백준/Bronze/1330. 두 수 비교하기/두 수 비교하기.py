fb6b74338e670737f44b937e437f6bd561263480
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 18:34:58 +0900

    Update 두 수 비교하기.py

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,3 +1,4 @@
+<1>
 a,b=map(int,input().split())
 if a>b:
     print(">")
@@ -5,3 +6,11 @@ elif a<b:
     print("<")
 else:
     print("==")
+
+<2>
+def solution(x,y):
+  return ">" if x>y else "==" if x==y else "<"
+
+#i/o
+a,b=map(int,input().split())
+print(solution(a,b))
