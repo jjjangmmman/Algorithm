136bc63f8c34ba15cb63e845fcd76455af4b4b8a
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 18:10:41 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,10 +1,4 @@
-def solution(A, X):
-  for a in A:
-    if a < X:
-      print(f"{a} " , end="")
-
-
-#i/o
-X = int(input().split()[1])
-A = list(map(int, input().split()))
-solution(A,X)
+l, std = map(int,input().split())
+arr = list(map(int,input().split()))
+result = [str(a) for a in arr if a<std]
+print(' '.join(result))
