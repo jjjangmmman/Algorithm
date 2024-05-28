503d5af743ba4273cbb58d7b6f6e7764e9a84d1d
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-05-28 21:05:24 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,17 @@
+def solution(n,arr):
+  answer = [[0 for b in range(100)] for a in range(100)]
+  for index in range(n):
+    x,y= arr[index]
+    for i in range(x,x+10):
+      for j in range(y,y+10):
+        answer[i][j] = 1
+  return sum( sum(row) for row in answer)
+    
+        
+# i/o
+import sys
+lines = sys.stdin.readlines()
+n = int(lines[0].strip())
+xy = [list(map(int,line.strip().split())) for line in lines[1:]]
+
+print(solution(n,xy))
