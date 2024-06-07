753d767599c0280b04a3262ddae0fe3ac3e2e091
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-06-08 04:43:58 +0900

    Update 문자열 집합.py

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -13,3 +13,16 @@ tmp = sys.stdin.readlines()
 n,m = map(int,tmp[0].strip().split())
 lines = tmp[1:]
 print(solution(n,lines))
+
+# idolphin씨의 코드
+from sys import stdin
+input = stdin.readline
+N, M = map(int, input().split())
+nSet = {input().rstrip() for _ in range(N)}
+answer = 0
+for _ in range(M):
+    key = input().rstrip()
+    if key in nSet:
+        answer += 1
+print(answer)
+키값이 필요없을때는 딕셔너리를 쓰지않고 집합 컴프리헨션으로 간단하게 처리하는게 매우 인상적
