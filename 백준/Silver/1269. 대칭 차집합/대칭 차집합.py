9dc0a436420874dfd550b732bc21bec73c883b9c
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-06-08 05:41:39 +0900

    [Silver IV] Title: 대칭 차집합, Time: 204 ms, Memory: 107580 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,7 +1,7 @@
 def solution(A,B):
     a = set(A.split())
     b = set(B.split())
-    return len((a|b) - (a&b)) #a.symmetric_difference(b)
+    return len(a.symmetric_difference(b))
     
 # i/o
 input()
