5972faa43820a9de6a5b37152bc6d551bfd4f922
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-06-08 20:43:26 +0900

    [Silver V] Title: 카드1, Time: 68 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -3,10 +3,10 @@ from collections import deque
 def solution(N):
     ret=[]
     q = deque(range(1,N+1))
-    while q:
+    for _ in range(N-1):
         ret.append(q.popleft())
-        if q:
-            q.append(q.popleft())
+        q.append(q.popleft())
+    ret.append(q.popleft())
     print(*ret)
 # i/o
 N = int(input())
