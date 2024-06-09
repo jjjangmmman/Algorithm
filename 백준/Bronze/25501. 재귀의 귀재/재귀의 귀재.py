e5098e87c500e4f1b650c5b9cd75325937a6cae7
najeong <167349165+jjjangmmman@users.noreply.github.com>
2024-06-09 21:47:32 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 352 ms, Memory: 31180 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -1,35 +1,23 @@
-"""
-1. 아이디어
-- 재귀함수 안에서, s[0]과 s[-1]이 같으면 다음 비교를 위해 재귀 호출
-- 문자열의 front가 rear와 같거나 커지면 모든 문자열의 팰린드롬 여부를 확인했으므로 return 1
-- s[front]와 s[rear]가 다르면 팰린드롬이 아니므로 return 0
-
-2. 시간 복잡도
-- O(T * n) > 가능
-
-3. 자료구조
-- 호출 횟수를 셀 cnt 변수
-- 반환값과 호출 횟수를 저장할 rs[]
-
-"""
-def recur(s,f,r,cnt):
+def recur(s,f,r):
+    global cnt
+    cnt+=1
     if f >= r: 
-        return [1,cnt]
+        return 1,cnt
     elif s[f] != s[r]: 
-        return [0,cnt]
+        return 0,cnt
     else: 
-        cnt+=1
-        return recur(s, f+1, r-1,cnt)
+        return recur(s, f+1, r-1)
 
-def solution(T,rs):
+def solution(T):
+    global cnt
     for _ in range(T):
+        cnt = 0
         s = input().rstrip()
-        rs = recur(s,0,len(s)-1,1)
-        print(*rs) 
+        result, count = recur(s,0,len(s)-1)
+        print(result, count) 
     
 import sys
 input = sys.stdin.readline
 
 T = int(input())
-rs = []
-solution(T,rs)
+solution(T)
