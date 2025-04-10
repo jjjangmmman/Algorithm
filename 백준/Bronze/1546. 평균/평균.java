96838b487d7cc93be5102f3aec6de1fa73588239
najeong <167349165+jjjangmmman@users.noreply.github.com>
2025-04-10 22:02:57 +0900

    [Bronze I] Title: 평균, Time: 204 ms, Memory: 18760 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -5,15 +5,15 @@ public class Main{
         Scanner sc  = new Scanner(System.in);
         int N = sc.nextInt();
         int A[] = new int[N];
-        for(int i =0;i<A.length;i++){
+        for(int i =0;i<N;i++){
             A[i] = sc.nextInt();
         }
         int sum = 0;
         int max = 0;
-        for(int i=0;i<A.length;i++){
+        for(int i=0;i<N;i++){
             if(A[i]>max) max=A[i];
             sum+=A[i];
         }
-        System.out.println((double)sum*100/max/N);
+        System.out.println((double)sum*100/(max*N));
     }
 }
