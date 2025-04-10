cad46d82265412aaaef55a059192db3dc633afd0
najeong <167349165+jjjangmmman@users.noreply.github.com>
2025-04-10 22:54:52 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 500 ms, Memory: 53972 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,16 +8,21 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         int suNo = Integer.parseInt(st.nextToken());
         int quizNo = Integer.parseInt(st.nextToken());
+        
         long[] S = new long[suNo+1];
+        
         st = new StringTokenizer(br.readLine());
         for(int i=1;i<=suNo;i++){
             S[i] = S[i-1] + Integer.parseInt(st.nextToken());
         }
+        
+        StringBuilder sb = new StringBuilder();
         for(int q=0;q<quizNo;q++){
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            System.out.println(S[j] - S[i-1]);
+            sb.append(S[j] - S[i-1]).append('\n');
         }
+        System.out.print(sb);
     }
 }
